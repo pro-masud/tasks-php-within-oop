68b6c295839ce029af2118deb7e500c9860d8c1d
pro-masud <promasudbd@gmail.com>
2024-10-09 13:05:38 +0600

    feat: new learing OOP be agen

--- index.php
+++ index.php
@@ -1,75 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tasks Project Within OOP</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
-</head>
-<body>
+<?php 
+    class Shape{
 
-    <div class="container">
-        <div class="row">
-            <div class="column column-70 column-offset-30">
-                <div class="student-admin" style="margin-top: 100px;">
-                    <h1>Simple Auth Examples</h1>   
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est vel voluptates, repellendus blanditiis veritatis ex distinctio et tenetur saepe tempore?</p>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="column column-70 column-offset-30">
-                <div class="student-admin">
-                    <h4>Upcoming Tasks</h4>   
-                    <form action="">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th>id</th>
-                                    <th>Task Name</th>
-                                    <th>Date</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td><input type="checkbox" value="1"></td>
-                                    <td>1</td>
-                                    <td>PHP Development</td>
-                                    <td>20 May, 2020</td>
-                                    <td><a style="color:red;" href="">Delete</a> || <a style="color: green;" href="">Complate Task</a></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <select style="width: 15%" name="" id="">
-                            <option value="">--Select--</option>
-                            <option value="1">One</option>
-                        </select>
-                        <input type="submit" value="Submit">
-                    </form>
-                </div>
-            </div>
-        </div>
-        <p>...</p>
-        <div class="row">
-            <div class="column column-70 column-offset-30">
-                <h2>Add New Task</h2>
-                <form action="">
-                    <label for="">Task Name</label>
-                    <input type="text" name="task">
-                    <label for="">Date</label>
-                    <input type="text" name="date">
-                    <label for="">Date</label>
-                    <input type="submit" value="Submit">
-                </form>
-            </div>
-        </div>
-    </div>
-    
-</body>
-    
-</body>
-</html>
+    }
+    class Shapes{
+        private $shapes;
+
+        public function __construct(){
+            $this -> shapes = [];
+        }
+
+        public function addShape( Shape $shape){
+            array_push( $this -> shapes, $shape );
+        }
+
+        public function totalShape(){
+            return count( $this -> shapes );
+        }
+    }
+
+    class Rectangle extends Shape{
+
+    }
+
+    class Triangle extends Shape{
+
+    }
+
+    class Student {
+        
+    }
+
+    $newShapes = new Shapes();
+    $newShapes -> addShape( new Rectangle());
+    $newShapes -> addShape( new Triangle());
+
+    echo  $newShapes -> totalShape();
